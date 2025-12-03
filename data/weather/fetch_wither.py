9d38f70cd762705419fe82e5574fde928fea0efd
clonda-cmd <126339414+clonda-cmd@users.noreply.github.com>
2025-12-04 00:22:06 +0300

    Add weather data fetching script and generated weather.csv

---
+++ data/weather/fetch_wither.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import requests
+import os
+
+# Путь к вашей папке
+folder = r"C:\Users\mezoh\OneDrive\Desktop\scada-fault-analysis\data\weather"
+filename = os.path.join(folder, "weather.csv")
+
+# URL API
+url = "https://api.open-meteo.com/v1/forecast?latitude=47.18078639647388&longitude=39.647388&hourly=temperature_2m&timezone=Europe%2FMoscow&start_date=2025-11-11&end_date=2025-11-25"
+
+# Создаем папку если её нет
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+# Получаем и сохраняем данные
+response = requests.get(url)
+data = response.json()
+
+weather_df = pd.DataFrame({
+    'datetime': pd.to_datetime(data['hourly']['time']),
+    'temperature': data['hourly']['temperature_2m']
+})
+
+weather_df.to_csv(filename, index=False)
+print(f"✅ Сохранено {len(weather_df)} строк в {filename}")
